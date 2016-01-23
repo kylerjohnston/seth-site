239a875641a4f5281880be554ab61b363374a311
kylerjohnston <kjhnstn2@illinois.edu>
2016-01-23 17:53:28 -0600

    first full draft of the site

--- seth_site/views.py
+++ seth_site/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/issues')
+def platform():
+    return render_template('issues.html')
