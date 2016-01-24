6c889035fa7d30347af0ef6dd3fba891f9f8e994
kylerjohnston <kjhnstn2@illinois.edu>
2016-01-24 10:50:02 -0600

    Removed flask-flatpages dependencies (not using it)

--- seth_site/__init__.py
+++ seth_site/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 from config import config
 
-flatpages = FlatPages()
 freezer = Freezer()
 
 app = Flask(__name__)
 app.config.from_object(config['dev'])
 config['dev'].init_app(app)
-flatpages.init_app(app)
 freezer.init_app(app)
 
 import seth_site.views
