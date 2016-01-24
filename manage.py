2b32b9ebb9f84f91ffdb1496d8064fd2589e19d6
kylerjohnston <kjhnstn2@illinois.edu>
2016-01-24 09:43:39 -0600

    Added tar to manage.py

--- manage.py
+++ manage.py
@@ -4,13 +4,18 @@ from seth_site import app, freezer
 from flask.ext.script import Manager
 import os
 from config import config
+import subprocess
 
 manager = Manager(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 @manager.command
 def build():
     app.config.from_object(config['production'])
     freezer.freeze()
+    subprocess.run(['/usr/bin/tar', '-zcvf',
+                    '{}/seth_site.tar.gz'.format(basedir),
+                    '{}/seth_site/build/'.format(basedir)])
 
 if __name__ == '__main__':
     manager.run()
