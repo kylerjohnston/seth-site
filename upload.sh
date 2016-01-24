bae6adafa7bf63c429e933b9e9d7306600a080a7
kylerjohnston <kjhnstn2@illinois.edu>
2016-01-24 13:13:59 -0600

    Upload over sftp

--- upload.sh
+++ upload.sh
@@ -9,13 +9,11 @@
 #
 # Copied largely unmodified from StackOverflow:
 # http://serverfault.com/questions/24622/how-to-use-rsync-over-ftp
-#
-# Todo: Enable SSL
 
 HOST="sethbaker.me"
 USER=$1
 PASS=$2
-FTPURL="ftp://$USER:$PASS@$HOST"
+FTPURL="sftp://$USER:$PASS@$HOST"
 LCD=seth_site/build
 RCD=public_html/
 DELETE="--delete"
@@ -23,8 +21,8 @@ DELETE="--delete"
 echo $FTPURL
 
 lftp -c "set ftp:list-options -a;
-set ftp:ssl-allow no;
-open '$FTPURL';
+set sftp:auto-confirm true;
+open -p 21098 '$FTPURL';
 lcd $LCD;
 cd $RCD;
 mirror --reverse \
