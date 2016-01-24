2f5febdb51bb6e37efeae1765e9985c4da82adcb
kylerjohnston <kjhnstn2@illinois.edu>
2016-01-24 13:07:50 -0600

    Got rid of tarball stuff; replaced with lftp script upload.sh

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from seth_site import app, freezer
 from flask.ext.script import Manager
 import os
 from config import config
-import subprocess
 
 manager = Manager(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -13,11 +12,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 def build():
     app.config.from_object(config['production'])
     freezer.freeze()
-    subprocess.run(['/usr/bin/tar', '-zcvf',
-                    '{}/seth_site.tar.gz'.format(basedir),
-                    '-C',
-                    '{}/seth_site/build/'.format(basedir),
-                    '.'])
 
 if __name__ == '__main__':
     manager.run()
