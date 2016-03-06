5ff18e1f94b065d0815f90a043afaf8c0b526c44
kylerjohnston <kjhnstn2@illinois.edu>
2016-03-05 20:27:37 -0600

    Creates database model

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     @staticmethod
     def init_app(app):
         pass
 
 class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'testing.db')
     DEBUG = True
     TESTING = False
 
--- manage.py
+++ manage.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
-from seth_site import create_app
-from flask.ext.script import Manager
+from seth_site import create_app, db
+from seth_site.models import User, Post
+from flask.ext.script import Manager, Shell
 import os
 from config import config
 
 app = create_app(os.getenv('SETHSITE_CONFIG') or 'default')
 manager = Manager(app)
-basedir = os.path.abspath(os.path.dirname(__file__))
+
+def make_shell_context():
+    return dict(
+        app = app,
+        db = db,
+        User = User,
+        Post = Post)
+
+manager.add_command('shell',
+                    Shell(make_context = make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
