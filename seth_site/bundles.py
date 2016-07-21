65252847bc06faafafafe4b1b06b7e391d8a4d78
kylerjohnston <kjhnstn2@illinois.edu>
2016-07-21 18:11:12 -0400

    Eliminates extra font icons

--- seth_site/bundles.py
+++ seth_site/bundles.py
@@ -5,6 +5,10 @@ css_all = Bundle('css/bootstrap.min.css',
                  filters = 'scss,cssutils',
                  output = 'gen/compiled.css')
 
+fontawesome = Bundle('font-awesome-4.6.3/scss/font-awesome.scss',
+                     filters = 'scss,cssutils',
+                     output = 'gen/fontawesome.css')
+
 js_all = Bundle('js/jquery-1.9.1.min.js',
                 'js/bootstrap.min.js',
                 output = 'gen/compiled.js')
